50a0c352f150ced2a9e81dd34c2067641f32a4be
Kapil <kapilsoni54768161@gmail.com>
2024-06-01 20:14:03 +0530

    done with the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,13 +20,13 @@ export default function Home() {
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your image on a{" "}
                 <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
-                Laptop Case
+                iPhone Case
               </h1>
               <p className="mt-8 text-g lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favourite memories with your own,{" "}
                 <span className="font-semibold">one-of-one </span>
-                laptop-case. CaseCobra LapSnap allows you to protect your
-                memories, not just your laptop case.
+                iPhone-case. Caseify allows you to protect your memories, not
+                just your iPhone.
               </p>
               <ul className="mt-8 space-y-2 text-left font-medium flex  flex-col items-center sm:items-start">
                 <div className="space-y-2">
